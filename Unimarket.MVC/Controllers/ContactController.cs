ecf037e9f75e4e76e138e9e9dc7e5583344c7049
cuongphan <cuongpvpvc@gmail.com>
2024-05-29 04:19:20 +0700

    Form_UI

---
+++ Unimarket.MVC/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unimarket.MVC.Controllers
+{
+	public class ContactController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
