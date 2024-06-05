e4d81873a7ea59d02f33f541f058857e008c3f8b
cuongphan <cuongpvpvc@gmail.com>
2024-06-05 07:15:06 +0700

    OrderController

---
+++ Unimarket.MVC/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unimarket.MVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
