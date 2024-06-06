c202b63372fcdda6015e5d903c113254348cd4c2
cuongphan <cuongpvpvc@gmail.com>
2024-06-07 04:20:03 +0700

    OrderHitory and OrderDetails

--- Unimarket.MVC/Controllers/ProfileController.cs
+++ Unimarket.MVC/Controllers/ProfileController.cs
@@ -1,20 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Unimarket.MVC.Helpers;
+using Unimarket.MVC.Models.ViewModels;
+using Unimarket.MVC.Services;
 
 namespace Unimarket.MVC.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly IHttpClientFactory _factory;
+        private readonly ILogger<ProfileController> _logger;
+        private readonly HttpClient _client;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ICurrentUserService _currentUserService;
+        public ProfileController(ILogger<ProfileController> logger, IHttpClientFactory httpClientFactory,
+            IConfiguration configuration, ICurrentUserService currentUserService)
+        {
+            _factory = httpClientFactory;
+            _client = new HttpClient();
+            _currentUserService = currentUserService;
+            _client = _factory.CreateClient("ServerApi");
+            _client.BaseAddress = new Uri(configuration["Cron:localhost"]);
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult OrderDetails()
+        [HttpGet]
+        public async Task<IActionResult> OrderHistory([FromQuery] DefaultSearch defaultSearch)
         {
-            return View();
+            var userId = HttpContext.Session.GetString("UserId");
+            ResponseOrder orders = new ResponseOrder();
+            var response = await _client.GetAsync(_client.BaseAddress + $"Order/user?userId={userId}");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                orders = JsonConvert.DeserializeObject<ResponseOrder>(data);
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+            return View(orders);
         }
-        public IActionResult OrderHistory()
+        public async Task<IActionResult> OrderDetails([FromQuery] DefaultSearch defaultSearch, [FromQuery] string orderId)
         {
-            return View();
+            var userId = HttpContext.Session.GetString("UserId");
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+
+            ResponseOrder orders = new ResponseOrder();
+            var response = await _client.GetAsync(_client.BaseAddress + $"Order/user?userId={userId}");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                orders = JsonConvert.DeserializeObject<ResponseOrder>(data);
+
+                var order = orders.Data.FirstOrDefault(o => o.Id == Guid.Parse(orderId));
+                if (order == null)
+                {
+                    return NotFound();
+                }
+
+                return View(order); 
+            }
+            else
+            {
+                return RedirectToAction("OrderHistory");
+            }
         }
+
     }
 }
