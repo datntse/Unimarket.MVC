8b991927d777611d5bb6cf5d2fb08135cb872914
cuongphan <cuongpvpvc@gmail.com>
2024-06-10 06:22:14 +0700

    shop

--- Unimarket.MVC/Controllers/ShopController.cs
+++ Unimarket.MVC/Controllers/ShopController.cs
@@ -1,12 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Unimarket.MVC.Helpers;
+using Unimarket.MVC.Models.ViewModels;
+using Unimarket.MVC.Services;
 
 namespace Unimarket.MVC.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _factory;
+        private readonly ILogger<ShopController> _logger;
+        private readonly HttpClient _client;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ICurrentUserService _currentUserService;
+        public ShopController(ILogger<ShopController> logger, IHttpClientFactory httpClientFactory,
+            IConfiguration configuration, ICurrentUserService currentUserService)
         {
-            return View();
+            _factory = httpClientFactory;
+            _client = new HttpClient();
+            _currentUserService = currentUserService;
+            _client = _factory.CreateClient("ServerApi");
+            _client.BaseAddress = new Uri(configuration["Cron:localhost"]);
         }
+        [HttpGet]
+        public async Task<IActionResult> Index(DefaultSearch defaultSearch)
+        {
+            ResponseProductVM productList = new ResponseProductVM();
+
+            // Construct the query string
+            var queryParams = new List<string>
+            {
+                $"perPage={1}",
+                $"currentPage={defaultSearch.currentPage}",
+                $"sortBy={defaultSearch.sortBy}",
+                $"isAscending={defaultSearch.isAscending}"
+            };
+
+            // Add category names to the query string
+            if (defaultSearch.categoryNames != null && defaultSearch.categoryNames.Any())
+            {
+                queryParams.AddRange(defaultSearch.categoryNames.Select(c => $"categoryNames={Uri.EscapeDataString(c)}"));
+            }
+
+            // Add price range to the query string
+            if (defaultSearch.MinPrice.HasValue)
+            {
+                queryParams.Add($"minPrice={defaultSearch.MinPrice.Value}");
+            }
+            if (defaultSearch.MaxPrice.HasValue)
+            {
+                queryParams.Add($"maxPrice={defaultSearch.MaxPrice.Value}");
+            }
+
+            var queryString = string.Join("&", queryParams);
+            var response = await _client.GetAsync(_client.BaseAddress + $"Item?{queryString}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                productList = JsonConvert.DeserializeObject<ResponseProductVM>(data);
+            }
+
+            return View(productList);
+        }
+
     }
 }
--- Unimarket.MVC/Helpers/DefaultSearch.cs
+++ Unimarket.MVC/Helpers/DefaultSearch.cs
@@ -6,5 +6,8 @@
         public int currentPage { get; set; } = 0;
         public String? sortBy { get; set; }
         public bool isAscending { get; set; } = true;
+        public List<string> categoryNames { get; set; }
+        public float? MinPrice { get; set; }
+        public float? MaxPrice { get; set; }
     }
 }
