ff648af1618adbe37aec45f1fb883bc7432042f1
Anh Duy <duybtvdu12354@gmail.com>
2024-11-23 10:44:51 +0700

    new

---
+++ Unimarket.MVC/Handlers/ErrorHandlingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace Unimarket.MVC.Handlers
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+
+            // Kiểm tra mã trạng thái
+            if (context.Response.StatusCode == 405)
+            {
+                // Điều hướng về trang Index của Home
+                context.Response.Redirect("/Home");
+            }
+        }
+    }
+}
