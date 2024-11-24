21bfdd58e4f445b23497e1f64e97c33f77b7a178
datnt <dat.nt271102@gmail.com>
2024-11-24 15:28:33 +0700

    fix mua hang

--- Unimarket.MVC/Handlers/ErrorHandlingMiddleware.cs
+++ Unimarket.MVC/Handlers/ErrorHandlingMiddleware.cs
@@ -2,23 +2,23 @@
 {
     public class ErrorHandlingMiddleware
     {
-        //private readonly RequestDelegate _next;
+        private readonly RequestDelegate _next;
 
-        //public ErrorHandlingMiddleware(RequestDelegate next)
-        //{
-        //    _next = next;
-        //}
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
 
-        //public async Task Invoke(HttpContext context)
-        //{
-        //    await _next(context);
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
 
-        //    // Kiểm tra mã trạng thái
-        //    if (context.Response.StatusCode == 405)
-        //    {
-        //        // Điều hướng về trang Index của Home
-        //        context.Response.Redirect("/");
-        //    }
-        //}
+            // Kiểm tra mã trạng thái
+            if (context.Response.StatusCode == 405)
+            {
+                // Điều hướng về trang Index của Home
+                context.Response.Redirect("/Home");
+            }
+        }
     }
 }
